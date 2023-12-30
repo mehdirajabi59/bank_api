7e42b1d9742c4ad0d666600e594c088cebf3cc30
Mehdi Rajabi <mehdints@gmail.com>
2023-12-30 22:59:11 +0330

    test: add some tests for transfer money

--- tests/Feature/TransferMoneyTest.php
+++ tests/Feature/TransferMoneyTest.php
@@ -4,34 +4,34 @@ namespace Tests\Feature;
 
 use App\Enums\TransferConfig;
 use App\Events\TransferSucceed;
-use App\Listeners\SendBalanceSms;
+use App\Exceptions\NoSufficientMoneyException;
 use App\Models\AccountCard;
-use App\Notifications\Sms\Contracts\SmsInterface;
 use App\Notifications\Sms\Operators\Kavehnegar;
 use App\Rules\IranianBankCardNumber;
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class TransferMoneyTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->seed();
 
-        app()->bind(IranianBankCardNumber::class, fn ()=> new IranianBankCardNumberMock());
-        app()->bind(Kavehnegar::class, fn() => new MockKavehnegar());
+        $mockIranianBankCardNumber = $this->mock(IranianBankCardNumber::class);
+        $mockIranianBankCardNumber->allows('validate')->andReturnTrue();
+        app()->instance(IranianBankCardNumber::class, $mockIranianBankCardNumber);
+
+        $mockIranianBankCardNumber = $this->mock(Kavehnegar::class);
+        $mockIranianBankCardNumber->allows('send')->andReturnTrue();
+        app()->instance(Kavehnegar::class, $mockIranianBankCardNumber);
     }
 
-    /**
-     * A basic feature test example.
-     */
     public function test_transfer_total_balance_expect_balance_is_zero(): void
     {
 
@@ -51,32 +51,82 @@ class TransferMoneyTest extends TestCase
             'amount' => $amountTransfer
         ]);
 
-        $response->assertStatus(200);
+        $response->assertOk()
+            ->assertJson([
+                'message' => __('transfer.successful')
+            ]);
 
         $sourceCard->refresh();
         $destCard->refresh();
 
-        $this->assertEquals($sourceCard->account->balance, 0);
+        $this->assertEquals(0, $sourceCard->account->balance);
         $this->assertEquals($destCard->account->balance, $amountTransfer);
 
         Event::assertDispatched(TransferSucceed::class);
     }
-}
 
-
-class MockKavehnegar implements SmsInterface
-{
-
-    public function send(string $phoneNumber, string $text)
+    public function test_transfer_money_with_persian_numbers(): void
     {
-        // TODO: Implement send() method.
+
+        Event::fake();
+
+        $amountTransfer = fake()->numberBetween(1000, 500000);
+
+        $sourceCard = AccountCard::factory()->hasAccount()->create();
+        $sourceCard->account->update(['balance' => $amountTransfer + TransferConfig::Fee->value]);
+
+        $destCard = AccountCard::query()->inRandomOrder()->where('account_id', '!=', $sourceCard->account_id)->first();
+        $destCard->account->update(['balance' => 0]);
+
+        $response = $this->postJson(route('transfer-money'), [
+            'source_card_number' => $this->englishNumberToPersian($sourceCard->card_number),
+            'dest_card_number' => $this->englishNumberToPersian($destCard->card_number),
+            'amount' => $this->englishNumberToPersian($amountTransfer)
+        ]);
+
+        $response->assertOk()
+            ->assertJson([
+                'message' => __('transfer.successful')
+            ]);
+
+        $sourceCard->refresh();
+        $destCard->refresh();
+
+        $this->assertEquals(0, $sourceCard->account->balance);
+        $this->assertEquals($destCard->account->balance, $amountTransfer);
+
+        Event::assertDispatched(TransferSucceed::class);
     }
-}
 
-class IranianBankCardNumberMock implements ValidationRule
-{
-    public function validate(string $attribute, mixed $value, Closure|\Closure $fail): void
+    public function test_transfer_fails_when_source_account_has_insufficient_funds(): void
     {
+        Event::fake();
+        $amountTransfer = fake()->numberBetween(1000, 500000);
+        $sourceCard = AccountCard::factory()->hasAccount()->create();
+        $sourceCard->account->update(['balance' => $amountTransfer]);
 
+        $destCard = AccountCard::query()->inRandomOrder()->where('account_id', '!=', $sourceCard->account_id)->first();
+        $destCard->account->update(['balance' => 0]);
+
+        $response = $this->postJson(route('transfer-money'), [
+            'source_card_number' => $sourceCard->card_number,
+            'dest_card_number' => $destCard->card_number,
+            'amount' => $amountTransfer
+        ]);
+
+        $response->assertBadRequest()
+            ->assertJson([
+                'message' => __('transfer.not-sufficient-money')
+            ]);
+
+        $sourceCard->refresh();
+        $destCard->refresh();
+
+        Event::assertNotDispatched(TransferSucceed::class);
+    }
+
+    private function englishNumberToPersian($enNumbers)
+    {
+        return str_replace([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'], $enNumbers);
     }
 }
