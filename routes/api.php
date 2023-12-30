105a77626252826feacc6531d4a9f0ee71f39287
Mehdi Rajabi <mehdints@gmail.com>
2023-12-30 22:58:31 +0330

    refactor: use alias

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/transfer-money', [\App\Http\Controllers\Account\TransferController::class, '__invoke'])
     ->name('transfer-money')
-    ->middleware(\App\Http\Middleware\CorrectNumberMiddleware::class .':source_card_number,dest_card_number,amount');
+    ->middleware( 'CorrectNumber:source_card_number,dest_card_number,amount');
 
 Route::get('recently-transactions', [\App\Http\Controllers\Account\RecentTransactionController::class, '__invoke']);
 
